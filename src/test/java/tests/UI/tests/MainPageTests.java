1aed2a3d8e49c397b21feecc8f47268d1eb494f0
OksanaIzi <73961325+OksanaIzi@users.noreply.github.com>
2021-07-12 16:22:53 +0300

    добавлен тест на VsegdaDa

--- src/test/java/tests/UI/tests/MainPageTests.java
+++ src/test/java/tests/UI/tests/MainPageTests.java
@@ -1,6 +1,7 @@
 package tests.UI.tests;
 
 import annotations.*;
+import com.codeborne.selenide.Condition;
 import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 import tests.UI.steps.MainPageSteps;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 @Layer("web")
 public class MainPageTests extends TestBase {
@@ -43,5 +48,21 @@ public class MainPageTests extends TestBase {
     public void checkSuccessfulOpenPage2(){
         steps.openMainPage(BASE_URL);
         steps.checkText();
+    }
+
+    @Test
+    public void openVsegdaDa(){
+        step("Открывам главную страницу магазина Детский мир", () -> {
+            open("https://vsegda-da.com/");
+        });
+        step("Кликаем на Акции", () -> {
+            $("a[href=\"https://cabinet.vsegda-da.com\"]").click();
+        });
+        step("Проверяем наличие текста ", () -> {
+            $("h3[data-component=\"Heading\"]").shouldHave(Condition.text("Вход в личный кабинет торговой организации"));
+        });
+        step("Вводим логин ", () -> {
+            $("input[id=\"login\"]").setValue("Тест");
+        });
     }
 }
